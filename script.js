7f1fcefecdafc3601aaafe1d3072df446d6e25ae
Weily <weily.lang@gmail.com>
2020-08-04 17:04:50 -0400

    added function to grab values from both menus

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // SET URL TO USE EITHER LOCAL OR DEPLOYED API
 const deployedFrontEnd = 'https://acpc-api.herokuapp.com';
-const URL = deployedFrontEnd ? deployedFrontEnd : 'https://acpc-api.herokuapp.com/';
+const URL = deployedFrontEnd ? deployedFrontEnd : 'https://localhost:3000';
 
 //////////////////////
 // GLOBAL VARIABLES
 //////////////////////
-const $selectAnimal = $("#select-animal");
-const $selectOutfit = $("#select-outfit");
-const $setFaveButton = $("#set-fave");
+//const $selectAnimal = $('#selectanimal');
+//const $selectOutfit = $('#selectoutfit');
+//const $setFaveButton = $('#setfave');
 // to add: Delete, Edit
 
 //////////////////////
@@ -18,23 +18,36 @@ const $setFaveButton = $("#set-fave");
 const populateAnimalMenu = async () => {
     const response = await fetch(`${URL}/animals`);
     const data = await response.json();
+    // console.log(data);
 
     data.forEach((animal) => {
         const $option = $('<option>').attr('value', animal._id).text(animal.name);
-        $selectAnimal.append($option);
+        $('#selectanimal').append($option);
+        // console.log(animal._id);
     })
 }
 
 // Get all outfits from API and populate the drop-down select
-const populateOutfitMenu =async () => {
+const populateOutfitMenu = async () => {
     const response = await fetch(`${URL}/outfits`);
     const data = await response.json();
 
     data.forEach((outfit) => {
         const $option = $('<option>').attr('value', outfit._id).text(outfit.name);
-        $selectOutfit.append($option);
+        $('#selectoutfit').append($('<option>').text("hello"));
+        $('#selectoutfit').append($option);
     })
 }
+
+// Make a card from the selected animal and outfit
+const makeCardFromMenus = async () => {
+    //const $selectedAnimal = $("#selectanimal").val();
+    //const $selectedOutfit = $("#selectoutfit").val();
+    `${$('#selectanimal').val() }+${$("#selectoutfit").val()}`
+    //console.log(`${$('#selectanimal').val() }+${$("#selectoutfit").val()}`);
+    console.log($("#selectanimal").val())
+}
+$("#setfave").on('click', makeCardFromMenus);
 
 const showAnimals = async (animals) => {
     animals.forEach(animal => {
@@ -48,6 +61,7 @@ const showAnimals = async (animals) => {
 //////////////////////
 // MAIN APPLICATION LOGIC
 //////////////////////
+
 // Get all animals
 populateAnimalMenu();
 // Get all outfits
