6b0d1deea2271d21ca88bf2fe374f6139f25228a
weilyl <weily.lang@gmail.com>
2020-08-03 23:25:31 -0400

    testing jQuery

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 // const $h1 = $('<h1>');
-const URL = 'https://acpc-api.herokuapp.com/'
+const deployedFrontEnd = 'https://acpc-api.herokuapp.com';
+const URL = deployedFrontEnd ? deployedFrontEnd : 'https://acpc-api.herokuapp.com/';
+
+const getAnimals = async () => {
+    const response = await fetch(`${URL}/animals`);
+    const data = await response.json();
+    showAnimals(animals)
+}
+
+const showAnimals = async (animals) => {
+    animals.forEach(animal => {
+        $li = $('<li>').html(`
+        <h1>${animal.name}</h1>
+        <img src="${animal.photo}" alt="${animal.photo-alt-text}">`);
+        $('body').append($li);
+    })
+}
 
 fetch(URL)
 .then(data => {
-    let $hello = $('<h1>').text("Hello world");
-    $('body').append($hello);
+    getAnimals()
 })
+
+// Hello world test
+// fetch(URL)
+// .then(data => {
+//     let $hello = $('<h1>').text("Hello world");
+//     $('body').append($hello);
+// })
